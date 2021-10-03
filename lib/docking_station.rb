12be709276709a54a2ac958683c1915e4e816f50
zainab baruud <zainabbaruud@ma373.home>
2021-10-03 21:02:09 +0100

    sorting out the bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,4 +6,11 @@ class DockingStation
     Bike.new
   end
 
+  def dock(bike)
+    @bike = bike
+  end
+
+  def bike
+    @bike
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,5 +11,17 @@ describe DockingStation do
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
-  
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike #after we've docked the bike, we need to return the bike we just docked
+  end
 end
