e1b60c9f8d9b978910f101cbafa194b0ebf86133
zainab baruud <zainabbaruud@ma373.home>
2021-10-03 13:21:59 +0100

    adding test to  make dockingstation get bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,9 @@ describe DockingStation do
   it 'responds to release bike' do
     expect(subject).to respond_to :release_bike
   end
+
+  it 'releases working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
