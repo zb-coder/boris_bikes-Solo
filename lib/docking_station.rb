76d74aca47761df12b96f51c76383c6c681118b9
zainab baruud <zainabbaruud@ma373.home>
2021-10-04 15:40:09 +0100

    Adding a guard condition to say there are no bikes available if there are no bikes docked at the docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,7 +4,8 @@ class DockingStation
 
   attr_reader :bike
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike #adding a guard condition
+    @bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,10 +4,16 @@ describe DockingStation do
   it 'responds to release bike' do
     expect(subject).to respond_to :release_bike
   end
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
 
-  it 'releases working bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
@@ -18,10 +24,11 @@ describe DockingStation do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
-
+  
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike #after we've docked the bike, we need to return the bike we just docked
   end
+
 end
