c94a2a28a824a4c72e325f7d1e3e63219a83d903
zainab baruud <zainabbaruud@ma373.home>
2021-10-19 15:28:40 +0100

    Adding private methods in order to adhere to the single responsibility principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,13 +7,22 @@ class DockingStation
   end
   
   def release_bike
-    fail 'No bikes available' if @bikes.empty? 
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking Station is full' if @bikes.count >= 20
+    fail 'Docking Station is full' if full?
     @bikes << bike
   end
 
+  private
+  
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty? 
+  end
 end
